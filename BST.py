d08ea30b431733d4fea460b98e2c4eb0a4b7a962
sjchakrav <sjchakrav@gmail.com>
2015-08-13 15:17:05 -0400

    add BST implementation with insert, find, preorder, and inorder

---
+++ BST.py
@@ -0,0 +1,100 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.leftChild = None
+		self.rightChild = None
+		
+	def insert(self, data):
+		if self.value == data:
+			return False
+			
+		elif self.value > data:
+			if self.leftChild:
+				return self.leftChild.insert(data)
+			else:
+				self.leftChild = Node(data)
+				return True
+
+		else:
+			if self.rightChild:
+				return self.rightChild.insert(data)
+			else:
+				self.rightChild = Node(data)
+				return True
+				
+	def find(self, data):
+		if(self.value == data):
+			return True
+		elif self.value > data:
+			if self.leftChild:
+				return self.leftChild.find(data)
+			else:
+				return False
+		else:
+			if self.rightChild:
+				return self.rightChild.find(data)
+			else:
+				return False
+
+	def preorder(self):
+		if self:
+			print (str(self.value))
+			if self.leftChild:
+				self.leftChild.preorder()
+			if self.rightChild:
+				self.rightChild.preorder()
+
+	def inorder(self):
+		if self:
+			if self.leftChild:
+				self.leftChild.inorder()
+			print (str(self.value))
+			if self.rightChild:
+				self.rightChild.inorder()
+
+class Tree:
+	def __init__(self):
+		self.root = None
+
+	def insert(self, data):
+		if self.root:
+			return self.root.insert(data)
+		else:
+			self.root = Node(data)
+			return True
+
+	def find(self, data):
+		if self.root:
+			return self.root.find(data)
+		else:
+			return False
+			
+	def preorder(self):
+		print("PreOrder")
+		self.root.preorder()
+			
+	def inorder(self):
+		print("InOrder")
+		self.root.inorder()
+
+#Instantiate a new tree
+bst = Tree()
+# print(bst.insert(7)) #=>boolean 
+bst.insert(3)
+bst.insert(1)
+bst.insert(21)
+bst.insert(15)
+bst.preorder()
+bst.inorder()
+
+
+
+
+
+
+
+
+
+
+
+
